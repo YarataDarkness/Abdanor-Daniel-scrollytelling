c13276fb75f3335ef93de9d3fd2c1adc4ff903a6
YarataDarkness <70409693+YarataDarkness@users.noreply.github.com>
2023-04-17 13:26:42 -0400

    g

--- script/script.js
+++ script/script.js
@@ -6,6 +6,8 @@ const istime = new seconds(0.1);
 
 
 iswindow.addEventListener('scroll' (), 
+    setTimeout(0,100),
+    //if setTimeout = (100), { isbody.classList.add('is-scrolling')}//
     isbody.classList.add('is-scrolling')
 );
 
