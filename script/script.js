c538b54dd539018b3a9c2f30261985f988b10593
YarataDarkness <70409693+YarataDarkness@users.noreply.github.com>
2023-04-17 13:10:42 -0400

    animation hell

--- script/script.js
+++ script/script.js
@@ -15,8 +15,11 @@ gsap.timeline({
         scrub: 1,
         markers:true,
         start: '50% bottom'
+
     }
+
 })
+
 
 .fromTo(
     document.querySelector('#god'),{
@@ -72,11 +75,19 @@ gsap.timeline({
 )
 
 .fromTo(
-    document.querySelector('#luci3'),{
+    document.querySelector('#luci2'),{
         opacity: '0%',
-        y:0,
+        x:0,
     },{
         opacity: '100%',
+        x:-100,
+    }
+)
+
+.fromTo(
+    document.querySelector('#god-2'),{
+        y: 0,
+    },{
         y: 100,
     }
 )
@@ -87,13 +98,178 @@ gsap.timeline({
         y:0,
     },{
         opacity: '100%',
+        y: 150,
+    }
+)
+
+.fromTo(
+    document.querySelector('#god-3'),{
+        y: 0,
+    },{
         y: 100,
     }
 )
 
 
+.fromTo(
+    document.querySelector('#luci4'),{
+        opacity: '0%',
+        x:0,
+    },{
+        opacity: '100%',
+        x:100,
+    }
+)
 
 
+.fromTo(
+    document.querySelector('#god-2'),{
+        y: 0,
+    },{
+        y: 100,
+    }
+)
 
 
+.fromTo(
+    document.querySelector('#luci5'),{
+        opacity: '0%',
+        y: 0,
+    },{
+        opacity: '100%',
+        y: 100,
+    }
+)
 
+
+.fromTo(
+    document.querySelector('#god-4'),{
+        y: 0,
+    },{
+        y: 100,
+    }
+)
+
+.fromTo(
+    document.querySelector('#luci6'),{
+        opacity: '0%',
+        x:0,
+    },{
+        opacity: '100%',
+        x:-100,
+    }
+)
+
+.fromTo(
+    document.querySelector('#god-2'),{
+        y: 0,
+    },{
+        y: 100,
+    }
+)
+
+.fromTo(
+    document.querySelector('#luci7'),{
+        opacity: '0%',
+        y: 0,
+    },{
+        opacity: '100%',
+        y: 150,
+    }
+)
+
+.fromTo(
+    document.querySelector('#luci8'),{
+        opacity: '0%',
+        y: 0,
+    },{
+        opacity: '100%',
+        y: 150,
+    }
+)
+
+.fromTo(
+    document.querySelector('#god-hand'),{
+        y: 50,
+    },{
+        y: -150,
+    }
+)
+
+.fromTo(
+    document.querySelector('#luci9'),{
+        opacity: '0%',
+        y: 0,
+    },{
+        opacity: '100%',
+        y: -10,
+        rotation: 180,
+    }
+)
+
+.fromTo(
+    document.querySelector('#luci10'),{
+        opacity: '0%',
+        y: 0,
+    },{
+        opacity: '100%',
+        y: -100,
+       
+    }
+)
+
+
+.fromTo(
+    document.querySelector('#luci11'),{
+        
+        y: 0,
+    },{
+        
+        y: -100,
+       
+    }
+)
+
+.fromTo(
+    document.querySelector('#luci12'),{
+        opacity: '0%',
+    },{
+        
+        opacity: '100%',
+       
+    }
+)
+
+
+.fromTo(
+    document.querySelector('#luci13'),{
+        opacity: '0%',
+    },{
+        
+        opacity: '100%',
+       
+    }
+)
+
+.fromTo(
+    document.querySelector('#luci13'),{
+        x:0,
+    },{
+        
+        x:-100,
+       
+    }
+)
+
+.fromTo(
+    document.querySelector('#dia-papa'),{
+        rotation: 50,
+        opacity: '0%',
+        y: 50,
+    },{
+        
+        rotation: 80,
+        opacity: '100%',
+        y: 150,
+    }
+)
