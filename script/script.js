ed7a2f240a2542c7aa61071b66760ad72a027fc7
YarataDarkness <70409693+YarataDarkness@users.noreply.github.com>
2023-04-20 13:50:28 -0400

    Update script.js

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ gsap.registerPlugin(ScrollTrigger);
 const iswindow = document.querySelector('window');
 const isbody = document.querySelector('body');
 const istime = new seconds(0.1);
-const toscroll = isbody.classList.add('is-scrolling');
+const toscroll = function(){isbody.classList.add('is-scrolling');}
 
 iswindow.addEventListener('scroll' (), 
     setTimeout(toscroll,100),
